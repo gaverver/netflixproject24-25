4e87af7e4115933565483f38ebfc3c6cc0816cc6
gaverver <gavrielsh06@gmail.com>
2024-11-26 12:16:39 +0200

    Create IMenu.h

---
+++ src/IMenu.h
@@ -0,0 +1,16 @@
+#ifndef IMENU_H
+#define IMENU_H
+
+#include <string>
+#include <vector>
+class IMenu {
+    public:
+        // pure virtual function to print a given string
+        virtual void print(std::string s) = 0;
+        // pure virtual function to scan input from user
+        virtual void scan() = 0;
+        // pure virtual function to process the command from user
+        virtual std::vector<std::string> nextCommand() = 0; 
+};
+
+#endif
