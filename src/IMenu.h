0cd3f8023d01370f0448598392be1ae5acf2527d
gaverver <gavrielsh06@gmail.com>
2024-11-26 18:09:07 +0200

    added a destructor to IMenu.h

--- src/IMenu.h
+++ src/IMenu.h
@@ -10,7 +10,9 @@ class IMenu {
         // pure virtual function to scan input from user
         virtual std::string scan() = 0;
         // pure virtual function to process the command from user
-        virtual std::vector<std::string> nextCommand() = 0; 
+        virtual std::vector<std::string> nextCommand() = 0;
+        // Virtual destructor to ensure proper cleanup in derived classes
+        virtual ~IMenu() = default;
 };
 
 #endif
