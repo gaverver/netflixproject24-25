c93c90655ee2ec9e84fe5cea4c7047b64355ad50
gaverver <167417630+gaverver@users.noreply.github.com>
2024-11-29 21:36:17 +0200

    Delete src/ICommand.cpp

--- src/ICommand.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <string>
-
-class ICommand {
-    public:
-    virtual void execute(const std::vector<std::string>& args) = 0;
-    virtual std::string description() const = 0;
-    virtual ~ICommand() = default;
-};
