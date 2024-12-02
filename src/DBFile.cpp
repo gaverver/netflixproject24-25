15899c3e094a55df95cad5efb7091bebb4dcb12e
ozero <ozerozer0809@gmail.com>
2024-12-02 22:50:48 +0200

    changing from int to unsigned long int and implement DBFile (without comments)

---
+++ src/DBFile.cpp
@@ -0,0 +1,217 @@
+#include "DBFile.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+//helper function to get the first word of a string
+std::string first_word(std::string s) {
+    //creating a stream to read from the string
+    std::istringstream stream(s);
+    std::string firstWord;
+    //reading the first word to a string
+    stream >> firstWord;
+    return firstWord;
+}
+
+//helper function to remove duplicates from sorted unsigned long int vector
+void removeDupSortVec(std::vector<unsigned long int>& vec) {
+    if (vec.empty()) return;
+
+    std::vector<unsigned long int> result;
+    result.push_back(vec[0]);
+
+    for (size_t i = 1; i < vec.size(); ++i) {
+        if (vec[i] != vec[i - 1]) {
+            result.push_back(vec[i]);
+        }
+    }
+
+    vec = std::move(result);
+}
+
+//helper function to merge a list of sorted ids to a line of sorted ids in file
+std::string mergeIds(const std::string& line, std::vector<unsigned long int>& ids) {
+    //remove duplicates from ids
+    removeDupSortVec(ids);
+    //define a stream for reading from the line
+    std::istringstream stream(line);
+    std::string mergedLine;
+    //reading the first word to the merged line (primary key)
+    stream >> mergedLine;
+    //reading the other numbers in the line
+    unsigned long int lineId;
+    stream >> lineId;
+    //i is index for iterating over ids
+    size_t i=0;
+    //id is ids[i]
+    unsigned long int id;
+    //merging the ids
+    while (!stream.eof() && i<ids.size()) {
+        id = ids[i];
+        if (id < lineId) {
+            //add the smaller id to the string
+            mergedLine += std::to_string(id) + " ";
+            i++;
+        } else if (id > lineId) {
+            //add the smaller id to the file
+            mergedLine += std::to_string(lineId) + " ";
+            stream >> lineId;
+        } else {
+            //add the smaller id to the string
+            mergedLine += std::to_string(id) + " ";
+            //there are no duplicates in line and in ids
+            i++;
+            stream >> lineId;
+        }
+    }
+    if (i >= ids.size()) {
+        while (!stream.eof()) {
+            mergedLine += std::to_string(lineId) + " ";
+            stream >> lineId;
+        }
+    }
+    while (i < ids.size()) {
+        id = ids[i];
+        mergedLine += std::to_string(id) + " ";
+        i++;
+    }
+
+
+    return mergedLine;
+}
+
+//modifiy file line to a new line
+void modifyFileLine(const std::string& filePath, unsigned long int lineNumber, const std::string& newLine) {
+    std::ifstream inputFile(filePath);
+
+    //read all the lines unsigned long into a vector
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        lines.push_back(line);
+    }
+    inputFile.close();
+    //modifiy the line
+    lines[lineNumber - 1] = newLine;
+    //write all the lines to the file
+    std::ofstream outputFile(filePath);
+
+    for (std::string mLine : lines) {
+        outputFile << mLine << "\n";
+    }
+    outputFile.close();
+
+}
+
+//creating constructor
+DBFile::DBFile(std::string path) : path(path) {
+    //userP is path to users.txt and moviesP is path to movies.txt
+    this->usersP = path + "/users.txt";
+    this->moviesP = path + "/movies.txt";
+}
+
+void genericUpdate(unsigned long int id, const std::vector<unsigned long int>& ids, std::string path) {
+    //create a copy of mids because it's a const
+    std::vector<unsigned long int> idsCopy;
+    idsCopy = std::move(ids);
+    //sort the mids first
+    std::sort(idsCopy.begin(), idsCopy.end());
+    std::ifstream inFile(path);
+    std::string line = "garbage";
+    unsigned long int k=0;
+    bool found = false;
+    while (std::getline(inFile, line)) {
+        k++;
+        if (first_word(line) == std::to_string(id)) {
+            found = true;
+            break;
+        }
+    }
+    inFile.close();
+    if (!found) {
+        std::ofstream outFile(path, std::ios::app);
+        outFile << id << " ";
+        for (unsigned long int i: idsCopy) {
+            outFile << i << " ";
+        }
+        outFile.close();
+    } else {
+        std::string modifiedLine = mergeIds(line, idsCopy);
+        modifyFileLine(path, k, modifiedLine);
+    }
+}
+
+void DBFile::updateUser(unsigned long int uid, const std::vector<unsigned long int>& mids) {
+    genericUpdate(uid, mids, usersP);
+    std::vector<unsigned long int> uidVec = {uid};
+    for (unsigned long int mid: mids) {
+        genericUpdate(mid, uidVec, moviesP);
+    }
+}
+
+void DBFile::updateMovie(unsigned long int mid, const std::vector<unsigned long int>& uids) {
+    genericUpdate(mid, uids, moviesP);
+    std::vector<unsigned long int> midVec = {mid};
+    for (unsigned long int uid: uids) {
+        genericUpdate(uid, midVec, usersP);
+    }
+}
+
+std::vector<unsigned long int> genericFind(unsigned long int id, std::string path) {
+    std::vector<unsigned long int> ids = {};
+    std::ifstream inFile(path);
+    std::string line = "garbage";
+    bool found = false;
+    while (std::getline(inFile, line)) {
+        if (first_word(line) == std::to_string(id)) {
+            found = true;
+            break;
+        }
+    }
+    if (found) {
+        unsigned long int idTemp;
+        std::istringstream stream(line);
+        //read the uneccesary first id (users/movies id)
+        stream >> idTemp;
+        //add the ids to the vector
+        while (stream >> idTemp) {
+            ids.push_back(idTemp);
+        }
+        return ids;
+    } else {
+        return ids;
+    }
+}
+
+//find movies user uid watched
+std::vector<unsigned long int> DBFile::findUser(unsigned long int uid) {
+    return genericFind(uid, usersP);
+}
+
+//find users that watched movie mid
+std::vector<unsigned long int> DBFile::findMovie(unsigned long int mid) {
+    return genericFind(mid, moviesP);
+}
+//return an unsigned long int vector of all the movie ids
+std::vector<unsigned long int> DBFile::getAllMovies() {
+    std::ifstream moviesInFile(moviesP);
+    std::vector<unsigned long int> mids = {};
+    std::string line;
+    std::istringstream stream;
+    unsigned long int mid;
+    while (std::getline(moviesInFile, line)) {
+        stream.str(line);
+        stream >> mid;
+        mids.push_back(mid);
+    }
+    return mids;
+}
+
+//clean the files in data
+void DBFile::cleanUp() {
+    std::ofstream users(usersP, std::ios::trunc);
+    users.close();
+    std::ofstream movies(moviesP, std::ios::trunc);
+    movies.close();
+}
---
+++ src/DBFile.h
@@ -0,0 +1,30 @@
+#ifndef DBFILE_H
+#define DBFILE_H
+#include "IDataBase.h"
+#include <vector>
+#include <string>
+class DBFile : public IDataBase {
+    private:
+        std::string path;
+        std::string usersP;
+        std::string moviesP;
+    public:
+        //constuctor to the class
+        DBFile(std::string path);
+        //pure virtual method to find movies that user watched
+        virtual std::vector<unsigned long int> findUser(unsigned long int uid) override;
+        //pure virtual method to users that watched a movie
+        virtual std::vector<unsigned long int> findMovie(unsigned long int mid) override;
+        //pure virtual method to add movies a user has watched
+        virtual void updateUser(unsigned long int uid, const std::vector<unsigned long int>& mids) override;
+        //pure virtual method to add users that watched a movie
+        virtual void updateMovie(unsigned long int mid, const std::vector<unsigned long int>& uids) override;
+        //pure virtual method to find common watched movies between 2 users
+        virtual std::vector<unsigned long int> getCommonMovies(unsigned long int uid1, unsigned long int uid2) override;
+        //pure virtual method to find all the movie ids of watched movies
+        virtual std::vector<unsigned long int> getAllMovies() override;
+        //method to clean the data from the files
+        void cleanUp();
+};
+
+#endif
