40e8b4eab69261b97db2dc4e047a081597735a7d
gaverver <gavrielsh06@gmail.com>
2024-12-13 15:07:37 +0200

    created executor interface

---
+++ src/executor.h
@@ -0,0 +1,12 @@
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+#include "Runnable.h"
+// executor interface
+class executor {
+public:
+    // pure virtual function: execute - executes the given runnable
+    virtual void execute(Runnable r) = 0;
+};
+
+#endif
