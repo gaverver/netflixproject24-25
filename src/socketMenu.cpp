2f91687a0d1f1627c81f037cda82b323736f1edc
ozero <ozerozer0809@gmail.com>
2024-12-13 17:22:13 +0200

    implement scan and print

--- src/socketMenu.cpp
+++ src/socketMenu.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <stdio.h>
 #include <iostream>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -11,5 +12,20 @@ socketMenu::socketMenu(int client_socket) : client_socket(client_socket) {
     bufferSize = sizeof(buffer);
 }
 
+std::string socketMenu::scan() {
+    int read_bytes =  recv(client_socket, buffer, bufferSize,0);
+    //connection never closed so read_bytes=0 dosen't close it
+    if (read_bytes < 0) {
+        //continue the program
+        return "";
+    }
+    //add \0 so the string knows where to stop
+    buffer[read_bytes] = '\0';
+    std::string s(buffer);
+    //return the string
+    return s;
+}
 
-
+void socketMenu::print(std::string output) {
+    send(client_socket, output.data(), output.size(), 0);
+}
