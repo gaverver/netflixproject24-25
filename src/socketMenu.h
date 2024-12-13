c315a8d1c142a5b06afd7d2340b21a2d8612f79d
ozero <ozerozer0809@gmail.com>
2024-12-13 17:05:09 +0200

    add socketMenu.h

---
+++ src/socketMenu.h
@@ -0,0 +1,21 @@
+#ifndef SOCKETMENU_H
+#define SOCKETMENU_H
+#include "IMenu.h"
+
+#include <string>
+#include <vector>
+// socketMenu class
+class socketMenu : public IMenu {
+    private:
+        int client_socket;
+    public:
+        //constructor for the class
+        socketMenu(int client_socket);
+        // method to send a given input to the client through the socket.
+        void print(std::string output) override;
+        // method to scan user command line from the socket.
+        std::string scan() override;
+        // method to get the command that the user entered.
+        std::vector<std::string>nextCommand () override;
+    };
+#endif
