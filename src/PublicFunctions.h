f5769ef0360d8c9670a48ba426437c64d455c131
gaverver <gavrielsh06@gmail.com>
2024-12-19 22:33:36 +0200

    added captureCommandOutput function

--- src/PublicFunctions.h
+++ src/PublicFunctions.h
@@ -2,8 +2,8 @@
 #define PUBLICFUNCTIONS_H
 
 #include <string>
-
+#include "ICommand.h"
 // helper function to convert from string to unsigned long int and know if it failed
 unsigned long int fromStringToULI(const std::string& str, bool& isValid);
-
+std::string captureCommandOutput(ICommand* Runner, const std::vector<std::string>& args);
 #endif
