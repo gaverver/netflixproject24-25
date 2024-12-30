f44a5db121d1eac97ce318d69a830031b5cea96e
ozero <ozerozer0809@gmail.com>
2024-12-30 16:52:42 +0200

    extract compare function to publicFunctions

--- src/PublicFunctions.h
+++ src/PublicFunctions.h
@@ -6,4 +6,8 @@
 // helper function to convert from string to unsigned long int and know if it failed
 unsigned long int fromStringToULI(const std::string& str, bool& isValid);
 std::string captureCommandOutput(ICommand* Runner, const std::vector<std::string>& args);
+//helper functio to remove the zeros at the beginning
+std::string removeZeros(const std::string& num);
+//helper function to compare strings which represents two huge numbers
+bool compareHugeNumbers(const std::string& num1, const std::string& num2);
 #endif
