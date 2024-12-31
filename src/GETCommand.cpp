d09a08c25bab4de10df09e550f39807a3c0bf527
AsifShlezinger <asifsh987@gmail.com>
2024-12-31 14:45:31 +0200

    fixed GET and it's tests to include ids as strings

--- src/GETCommand.cpp
+++ src/GETCommand.cpp
@@ -16,30 +16,19 @@ GETCommand::GETCommand(IDataBase& db, IMenu& menu) : db(db), menu(menu) {}
 
 // execute, inherited from Command class
 void GETCommand::execute(const std::vector<std::string>& args) {
-    // suppose to be only user id and movie id which are unsigned long int
+    // suppose to be only user id and movie id which are valid ids with type string
     if (args.size() != 2) {
         // error number - 400 - invalid command
         menu.print("400 Bad Request");
         return;
     }
 
-    // casting from string to unsigned long int, and returns if there was a problem doing so
-    bool isValid = true;
+    // for comfort
+    std::string uid = args[0];
+    std::string mid = args[1];
 
-    // casting the first argument from string to unsigned long int
-    unsigned long int uid = fromStringToULI(args[0], isValid);
-    // if the casting has failed, execute of GET should prints error code
-    if (!isValid) {
-        // error number - 400 - invalid command
-        menu.print("400 Bad Request");
-        return;
-    }
-
-    // casting the second argument from string to unsigned long int
-    unsigned long int mid = fromStringToULI(args[1], isValid);
-    // if the casting has failed, execute of GET should prints error code
-    if (!isValid) {
-        // error number - 400 - invalid command
+    // check if the ids of the user and the movie are valid (as an mongoDB _id object)
+    if (!IsValidId(uid) || !IsValidId(mid)) {
         menu.print("400 Bad Request");
         return;
     }
@@ -59,19 +48,19 @@ void GETCommand::execute(const std::vector<std::string>& args) {
     }
 
     // users who watched the movie 'mid'
-    std::vector<unsigned long int> alikeUsers = db.findMovie(mid);
+    std::vector<std::string> alikeUsers = db.findMovie(mid);
 
     // movies already watched by 'uid'
-    std::vector<unsigned long int> alreadyWatched = db.findUser(uid);
+    std::vector<std::string> alreadyWatched = db.findUser(uid);
 
     // movies and their ratings according to the algorithm (rate = relevance)
-    std::map<unsigned long int, int> rate;
+    std::map<std::string, int> rate;
 
     // get all movies in the database
-    std::vector<unsigned long int> allMovies = db.getAllMovies();
+    std::vector<std::string> allMovies = db.getAllMovies();
 
     // initialize the rate map for all eligible movies
-    for (unsigned long int movie : allMovies) {
+    for (std::string movie : allMovies) {
         // the movies that we are not supposed to recommend on is the mid and the movies uid already watched
         if (movie != mid && std::find(alreadyWatched.begin(), alreadyWatched.end(), movie) == alreadyWatched.end()) {
             rate[movie] = 0; // Initialize with 0
@@ -79,17 +68,17 @@ void GETCommand::execute(const std::vector<std::string>& args) {
     }
 
     // go through all the users that watched the movie mid
-    for (unsigned long int usr : alikeUsers) {
+    for (std::string usr : alikeUsers) {
         // skip uid
         if (usr == uid) continue;
 
         // calculate similarity of that user to uid according to the algorithm
         int similarity = db.getCommonMovies(usr, uid).size();
         // movies watched by current user - 'usr'
-        std::vector<unsigned long int> moviesOfAlikeUser = db.findUser(usr);
+        std::vector<std::string> moviesOfAlikeUser = db.findUser(usr);
 
         // go therough all the movies current 'usr' watched
-        for (unsigned long int movie : moviesOfAlikeUser) {
+        for (std::string movie : moviesOfAlikeUser) {
             // again, those are the movies that we are not supposed to recommend on
             if (movie == mid || std::find(alreadyWatched.begin(), alreadyWatched.end(), movie) != alreadyWatched.end()) {
                 continue;
@@ -100,15 +89,16 @@ void GETCommand::execute(const std::vector<std::string>& args) {
     }
 
     // sortedMovies will contain the 10 best movies we will recommend on
-    std::vector<unsigned long int> sortedMovies;
+    std::vector<std::string> sortedMovies;
     // at first, sortedMovies will have all the movies that have a rate
     for (const auto& pair : rate) {
         sortedMovies.push_back(pair.first);
     }
 
     // sort movies by rating and ID, (cmp function here is according to the algorithm)
-    std::sort(sortedMovies.begin(), sortedMovies.end(), [&rate](unsigned long int a, unsigned long int b) {
-        return rate[a] > rate[b] || (rate[a] == rate[b] && a < b);
+    std::sort(sortedMovies.begin(), sortedMovies.end(), [&rate](std::string a, std::string b) {
+        // compareHugeNumbers is from PublicFunctions to comapre between _id objects
+        return rate[a] > rate[b] || (rate[a] == rate[b] && compareHugeNumbers(a, b));
     });
 
     // take the top 10 (or less if there are less than 10 movies that have a rate) movies
