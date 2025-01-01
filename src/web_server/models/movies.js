a7094980abb4b46ddec5f2918ecb9a4fa064af6c
ozero <ozerozer0809@gmail.com>
2025-01-01 21:48:52 +0200

    add movies model

---
+++ src/web_server/models/movies.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Movie = new Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    actors: {
+        type: [String],
+        default: []
+    },
+    published: {
+        type: Date,
+        default: Date.now
+    },
+    age_limit: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 18
+    },
+    creators: {
+        type: [String],
+        required: true
+    },
+    categories: {
+        type: [Schema.Types.ObjectId],
+        ref: 'categories',
+        default: []
+    }
+});
+
+module.exports = mongoose.model('Movie', Movie);
