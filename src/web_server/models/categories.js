8fa590298a0af492781a35c4385987815b1c7bb5
AsifShlezinger <asifsh987@gmail.com>
2025-01-01 20:07:09 +0200

    added the model for category

---
+++ src/web_server/models/categories.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Category = new Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    promoted: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('categories', Category);
