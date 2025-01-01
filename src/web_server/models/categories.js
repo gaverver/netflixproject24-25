e101dc72ff454045dedc1f85a371094052c102c2
AsifShlezinger <asifsh987@gmail.com>
2025-01-01 21:54:34 +0200

    added movieIds field to the category model

--- src/web_server/models/categories.js
+++ src/web_server/models/categories.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Category = new Schema({
+    // the name of the category
     name : {
         type: String,
         required: true
     },
+    // if the category promoted or not
     promoted: {
         type: Boolean,
         default: false
+    },
+    // a list of movies ids of the movies that belong to this category
+    movieIds: {
+        type: [Schema.Types.ObjectId],
+        // reference to the 'movies' collection
+        ref: 'movies',
+        default: []
     }
 });
 
+// the name of the collection is categories
 module.exports = mongoose.model('categories', Category);
