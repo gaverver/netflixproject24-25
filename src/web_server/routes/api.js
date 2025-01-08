994347a57779f3c3a70055e12604c01eeec22399
ozero <ozerozer0809@gmail.com>
2025-01-08 21:30:22 +0200

    add ./

--- src/web_server/routes/api.js
+++ src/web_server/routes/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
 var router = express.Router();
 //import routers
-const categoriesRouter = require('api_routes/categories')
-const tokensRouter = require('api_routes/tokens')
-const moviesRouter = require('api_routes/movies')
-const usersRouter = require('api_routes/users')
+const categoriesRouter = require('./api_routes/categories')
+const tokensRouter = require('./api_routes/tokens')
+const moviesRouter = require('./api_routes/movies')
+const usersRouter = require('./api_routes/users')
 
 router.use('/categories', categoriesRouter)
 router.use('/tokens', tokensRouter)
