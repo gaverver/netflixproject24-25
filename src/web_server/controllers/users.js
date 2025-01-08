eb4ddbdc5d9a8bfc38b82ae3b82308e0b492ab78
gaverver <gavrielsh06@gmail.com>
2025-01-08 19:27:29 +0200

    create user controller

---
+++ src/web_server/controllers/users.js
@@ -0,0 +1,94 @@
+const userService = require("../services/users")
+
+
+const createUser = async (req, res) => {
+    const MAX_IMAGES = 10
+    // get all the necessary details.
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+    const phoneNumber = req.body.phoneNumber
+    const picture = req.body.picture
+    // check if a username was entered
+    if (username === undefined) {
+        return res.status(400).json({ error:'username is required' });
+    }
+    // check if the type of username is a string
+    if (typeof username !== 'string') {
+        return res.status(400).json({ error:'Invalid data: username must be a string' });
+    }
+
+    // check if a password was entered
+    if (password === undefined) {
+        return res.status(400).json({ error:'password is required' });
+    }
+    // check if the type of password is a string
+    if (typeof password !== 'string') {
+        return res.status(400).json({ error:'Invalid data: password must be a string' });
+    }
+
+    // check if an email was entered
+    if (email === undefined) {
+        return res.status(400).json({ error:'email is required' });
+    }
+    // check if the email is with the correct format.
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (emailRegex.test(email) === false) {
+        return res.status(400).json({ error:'email is in an invalid format' });
+    }
+
+    // check if a phoneNumber was entered
+    if (phoneNumber === undefined) {
+        return res.status(400).json({ error:'email is required' });
+    }
+    // check if the phoneNumber is with the correct format.
+    const phoneNumberRegex = /^(?:\+?\d{1,3}[-.\s]?)?(?:\(?\d{1,4}\)?[-.\s]?)?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+    if (phoneNumberRegex.test(phoneNumber) === false) {
+        return res.status(400).json({ error:'phone number is in an invalid format' });
+    }
+
+    if (picture === undefined) {
+        return res.status(400).json({ error:'picture is required'})
+    }
+
+    if (typeof picture !== 'number') {
+        return res.status(400).json({ error:'Invalid data: password must be a number' });
+    }
+
+    if (picture < 1 || picture > MAX_IMAGES) {
+        return res.status(400).json({ error:`picture must be between 1 and ${MAX_IMAGES}`})
+    }
+
+    
+
+    try {
+        // create the user using the service.
+        const response = userService.createUser(username, email, password, phoneNumber, picture)
+        // respond with the fitting response depending on the returned value
+        if (response === null) {
+            return res.status(404).json({ error:'username and/or email and/ or phoneNumber already exists'})
+        }
+        res.status(201).end()
+    } catch (error) {
+        // return error indicates that the server has crushed
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+}
+
+const getUserById = async (req, res) => {
+    const id = req.params.id
+    try {
+        const user = await userService.getUserById(id); 
+        // getUserById in the services returns null if the category wasn't found
+        if (!user) {
+            return res.status(404).json({ error:'User not found' });
+        }
+        // return the user and his details
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(404).json({ error:'User not found' });
+    }
+}
+
+module.exports = {getUserById, createUser }
