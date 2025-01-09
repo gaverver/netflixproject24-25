0672f58317b0e88e88d3510265f02d283f1f3f5d
ozero <ozerozer0809@gmail.com>
2025-01-09 19:01:34 +0200

    update categories according to utilities

--- src/web_server/services/categories.js
+++ src/web_server/services/categories.js
@@ -1,5 +1,5 @@
 const Category = require('../models/categories');
-const MovieService = require('../services/movies')
+const utilities = require('../services/utilities')
 
 const createCategory = async (name, promoted, movieIds) => {
     // check if there is a category with this name
@@ -45,11 +45,11 @@ const updateCategory = async (id, name, promoted, movieIds) => {
     // update in each new/removed movie the categories field
     // delete from each former movie the category from categories field
     for (const movie of formerMovies) {
-        await MovieService.deleteCategoryFromMovie(movie, id);
+        await utilities.deleteCategoryFromMovie(movie, id);
     }
     // add to each new movie the category to the categories field
     for (const movie of movieIds) {
-        await MovieService.addCategoryToMovie(movie, id);
+        await utilities.addCategoryToMovie(movie, id);
     }
     return category;
 };
@@ -61,34 +61,10 @@ const deleteCategory = async (id) => {
     if (!deletedCategory) return null;
     // else, delete the category from every movie that has it
     for (const movie of deletedCategory.movieIds) {
-        await MovieService.deleteCategoryFromMovie(movie, id);
+        await utilities.deleteCategoryFromMovie(movie, id);
     }
     return deletedCategory;
 };
 
-// 2 helper functions to use
-const addMovieToCategory = async (categoryId, movieId) => {
-    // get the category
-    const category = await getCategoryById(categoryId);
-    // check if the movie is not already there
-    if (!category.movieIds.includes(movieId)) {
-        // add the movieId to the movieIds array
-        category.movieIds.push(movieId);
-    }
-    await category.save();
-};
 
-const deleteMovieFromCategory = async (categoryId, movieId) => {
-    // get the category
-    const category = await getCategoryById(categoryId);
-    // get the index of the movie in the array
-    const index = category.movieIds.indexOf(movieId);
-    // check if the movie is there
-    if (index !== -1) {
-        // remove the movieId from the movieIds array
-        category.movieIds.splice(index, 1);
-    }
-    await category.save();
-};
-
-module.exports = { createCategory, getCategoryById, getCategories, updateCategory, deleteCategory, addMovieToCategory, deleteMovieFromCategory }
+module.exports = { createCategory, getCategoryById, getCategories, updateCategory, deleteCategory}
