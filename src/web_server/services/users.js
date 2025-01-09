f3541b2150b82ef3e2597bb495a8abce7d73933a
ozero <ozerozer0809@gmail.com>
2025-01-09 18:59:24 +0200

    update user according to utilities

--- src/web_server/services/users.js
+++ src/web_server/services/users.js
@@ -33,18 +33,4 @@ const getUserById = async (id) => {
     return await User.findById(id)
 }
 
-// helper function to add a watched movie - used by movies
-async function addMovieToUser(userId, movieId) {
-    const user = await User.find({_id: userId})
-    user.watched_movies.push(movieId)
-    await user.save();
-}
-
-// helper function to delete a watched movie - used by movies
-async function deleteMovieFromUser(userId, movieId) {
-    const user = await User.find({_id: userId})
-    user.watched_movies = watched_movies.filter(movie => movie !== movieId);
-    await user.save();
-}
-
-module.exports = {getUserById, createUser, addMovieToUser, deleteMovieFromUser }
+module.exports = {getUserById, createUser}
