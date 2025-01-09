e99971a50bd68dd0be0f2d2827f199aed88f035e
gaverver <gavrielsh06@gmail.com>
2025-01-09 12:37:38 +0200

    added error checker for url

--- src/web_server/app.js
+++ src/web_server/app.js
@@ -18,6 +18,12 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
 // Use the API router for all routes starting with /api
 app.use('/api', api);
+
+// Catch-all route for everything that doesn't start with /api
+app.all('*', (req, res) => {
+    res.status(404).json({ error: 'Not Found: invalid url request' });
+});
+
 // a variable with the ip and port of the recommendation system
 const recommendConString = process.env.RECOMMEND_CON_STRING
 // load the avatars for the clients.
