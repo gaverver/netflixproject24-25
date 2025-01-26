7e47f1524ec95bae7c9d10ef59c17bc48d0c397d
ozero <ozerozer0809@gmail.com>
2025-01-27 00:12:04 +0200

    add DBImage

---
+++ src/web_server/views/src/components/DBImage.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+
+const MovieImage = ({ photo }) => {
+  const [imageSrc, setImageSrc] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/images/${photo}`);
+        if (!response.ok) {
+          throw new Error('Image not found');
+        }
+
+        const imageJson = await response.json();
+        const { data, contentType } = imageJson;
+        const base64String = uint8ArrayToBase64(new Uint8Array(data.data));
+        const imageUrl = `data:${contentType};base64,${base64String}`;
+
+        setImageSrc(imageUrl);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error loading image:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchImage();
+
+  }, [photo]);
+
+  const uint8ArrayToBase64 = (uint8Array) => {
+    let binaryString = '';
+    for (let i = 0; i < uint8Array.length; i++) {
+      binaryString += String.fromCharCode(uint8Array[i]);
+    }
+    return window.btoa(binaryString);
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading image...</p>
+      ) : (
+        <img src={imageSrc} alt="Movie" />
+      )}
+    </div>
+  );
+};
+
+export default MovieImage;
