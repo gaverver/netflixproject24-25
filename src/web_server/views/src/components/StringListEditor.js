ff30fe2b6c12b58fd35671bdf8be5e5b1c41cad6
ozero <ozerozer0809@gmail.com>
2025-01-26 21:44:46 +0200

    add stringlisteditor

---
+++ src/web_server/views/src/components/StringListEditor.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+const StringListEditor = ({ list, setList, label, placeholder, itemName }) => {
+  const [newItem, setNewItem] = useState("");
+
+  useEffect(() => {
+    console.log("list updated:", list);
+  }, [list]);
+
+  const handleDelete = (index) => {
+    const updatedlist = list.filter((_, i) => i !== index);
+    setList(updatedlist);
+  };
+
+    const handleAdd = async () => {
+        const item = newItem.trim();
+        if (item !== "") {
+            setList([...list, item]);
+            setNewItem("");
+        }
+    };
+
+  return (
+    <div className="moviecat-editor">
+      <label>{label}</label>
+      <ul>
+        {list.map((str, index) => (
+          <li key={index}>
+            {str} 
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          onKeyPress={ (e) => {
+            if (e.key === "Enter") {
+                e.preventDefault();
+                handleAdd();
+            }
+            }}
+        />
+        <button onClick={handleAdd}>Add {itemName}</button>
+      </div>
+    </div>
+  );
+};
+
+export default StringListEditor;
