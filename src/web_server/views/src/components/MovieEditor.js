dd078d5474e9f475f76fdfd1e55e3923fbb87e8e
ozero <ozerozer0809@gmail.com>
2025-01-26 22:29:25 +0200

    alert bad requests

--- src/web_server/views/src/components/MovieEditor.js
+++ src/web_server/views/src/components/MovieEditor.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MoviecatEditor from "./MoviecatEditor";
+import StringListEditor from "./StringListEditor";
+import "./MovieEditor.css"
 
 const MovieEditor = ({id}) => {
     const [name, setName] = useState('');
@@ -53,13 +55,19 @@ const MovieEditor = ({id}) => {
                 },
                 body: JSON.stringify(updatedMovie),
             });
-        
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+            console.log(response);
+            if (response.status === 404 || response.status === 400) {
+                const errorData = await response.json();
+                alert(errorData.error || `Error: ${response.status}`);
+                return;
             }
+            if (!response.ok) {
+                alert("try again")
+                return
+            }
+            if (response.status === 204)
+                alert("update successfully")
         
-            const data = await response.json();
-            console.log("Movie updated successfully:", data);
         } catch (error) {
             console.error("Error updating movie:", error);
         }
@@ -96,7 +104,7 @@ const MovieEditor = ({id}) => {
                 />
                 </div>
 
-                <div>
+                {/* <div>
                 <label htmlFor="actors">Actors (comma-separated):</label>
                 <input
                     type="text"
@@ -104,7 +112,8 @@ const MovieEditor = ({id}) => {
                     value={actors.join(", ")}
                     onChange={(e) => setActors(e.target.value.split(",").map((a) => a.trim()))}
                 />
-                </div>
+                </div> */}
+                <StringListEditor list = {actors} setList = {setActors} label = {"Actors"} placeholder = {"New Actor"} itemName = {"actor"} />
 
                 <div>
                 <label htmlFor="age_limit">Age Limit:</label>
@@ -127,7 +136,7 @@ const MovieEditor = ({id}) => {
                 />
                 </div>
 
-                <div>
+                {/* <div>
                 <label htmlFor="creators">Creators (comma-separated):</label>
                 <input
                     type="text"
@@ -135,7 +144,9 @@ const MovieEditor = ({id}) => {
                     value={creators.join(", ")}
                     onChange={(e) => setCreators(e.target.value.split(",").map((c) => c.trim()))}
                 />
-                </div>
+                </div> */}
+
+                <StringListEditor list = {creators} setList = {setCreators} label = {"Creators"} placeholder = {"New Creator"} itemName = {"creator"} />
 
                 <MoviecatEditor categories={ categories } setCategories={ setCategories } />
 
