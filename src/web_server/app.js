0c3b7cf9b9032d2a42236e0586b5477cff70f31a
gaverver <gavrielsh06@gmail.com>
2025-01-28 00:41:04 +0200

    exposed location header

--- src/web_server/app.js
+++ src/web_server/app.js
@@ -1,40 +1,46 @@
-// Import all the required libraries
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// Import the router for handling API routes and images router
 const api = require('./routes/api');
-const imagesRouter = require('./routes/images')
-const videoRouter = require('./routes/videos')
-// Load environment variables from the configuration file based on the current environment
+const imagesRouter = require('./routes/images');
+const videoRouter = require('./routes/videos');
+
+// Load environment variables
 require('custom-env').env(process.env.NODE_ENV, './config');
-// Connect to MongoDB using the connection string from the environment variables
-mongoose.connect(process.env.CONNECTION_STRING,
-{ useNewUrlParser: true,
-useUnifiedTopology: true });
-// Initialize the Express application
+
+// Connect to MongoDB
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Initialize the Express app
 var app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({extended : true}));
+
+// CORS setup to expose the 'Location' header
+const corsOptions = {
+    exposedHeaders: ['Location'], // Expose 'Location' header to the client
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-// Use the API router for all routes starting with /api
+
+// Use the API router
 app.use('/api', api);
 
 // Use the images router
 app.use('/images', imagesRouter);
 app.use('/videos', videoRouter);
 
-// Catch-all route for everything that doesn't start with /api
+// Catch-all route
 app.all('*', (req, res) => {
     res.status(404).json({ error: 'Not Found: invalid url request' });
 });
 
-// a variable with the ip and port of the recommendation system
-const recommendConString = process.env.RECOMMEND_CON_STRING
-// load the avatars for the clients.
-app.use(express.static('../../data/avatars'))
-// run the server on the port from the configuration file
+// Server listening on configured port
+const recommendConString = process.env.RECOMMEND_CON_STRING;
 app.listen(process.env.PORT);
 
-module.exports = {recommendConString}
+module.exports = { recommendConString };
