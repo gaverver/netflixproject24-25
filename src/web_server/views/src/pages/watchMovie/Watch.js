69f43fd493b244be13bd0809f5af67d7d1827b57
ozero <ozerozer0809@gmail.com>
2025-02-01 00:08:48 +0200

    ass menu at watch page

--- src/web_server/views/src/pages/watchMovie/Watch.js
+++ src/web_server/views/src/pages/watchMovie/Watch.js
@@ -1,12 +1,16 @@
-// import "./Watch.css";
+import "./Watch.css";
 import React from "react";
 import { useNavigate, useParams } from "react-router";
 import VideoStreamer from "../../components/VideoStreamer";
+import Menu from "../../components/menu/menu";
 
 function Watch() {
     const {id} = useParams();
     return (
-        <VideoStreamer videoId={id} />
+        <div>
+            <Menu />
+            <VideoStreamer videoId={id} />
+        </div>
     );
 }
 
