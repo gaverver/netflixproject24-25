cabda27e4ac94517302b0b55a959abcde59712ba
gaverver <167417630+gaverver@users.noreply.github.com>
2025-02-01 20:38:17 +0200

    Update TokenWebServiceAPI.java

--- src/android/app/src/main/java/com/example/netflix/tokens/TokenWebServiceAPI.java
+++ src/android/app/src/main/java/com/example/netflix/tokens/TokenWebServiceAPI.java
@@ -5,8 +5,8 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 public interface TokenWebServiceAPI {
-    @GET("/api/tokens/{id}")
-    Call<String> getIdFromToken(@Path("id") String id);
+    @GET("/api/tokens/{token}")
+    Call<String> getIdFromToken(@Path("token") String id);
     @POST("/api/tokens")
     Call<String> createToken(@Body TokenRequest token);
 }
