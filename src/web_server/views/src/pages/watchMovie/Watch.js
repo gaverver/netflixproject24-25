3f391d8a436a2222c099d77dc51c6309f3930c28
ozero <ozerozer0809@gmail.com>
2025-02-01 19:35:31 +0200

    add back button from the video

--- src/web_server/views/src/pages/watchMovie/Watch.js
+++ src/web_server/views/src/pages/watchMovie/Watch.js
@@ -1,15 +1,23 @@
 import "./Watch.css";
 import React from "react";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
 import VideoStreamer from "../../components/VideoStreamer";
-import Menu from "../../components/menu/menu";
+import { useNavigate } from "react-router-dom";
 
 function Watch() {
     const {id} = useParams();
+    const navigate = useNavigate();
+    
     return (
         <div className="watchPage">
-            <Menu />
-            <VideoStreamer videoId={id} />
+            <button
+            className="btn btn-light position-absolute top-0 end-0 m-3 border rounded-circle shadow"
+            id = "backhome"
+            onClick={() => navigate("/")}
+            >
+            <i className="bi bi-arrow-left"></i>
+            </button>
+            <VideoStreamer videoId={id}/>
         </div>
     );
 }
