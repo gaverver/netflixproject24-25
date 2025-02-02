5de9d422ec69a46ee2c8d45d349e1e7db8475799
gaverver <167417630+gaverver@users.noreply.github.com>
2025-02-02 22:56:47 +0200

    removed unecassary fields

--- src/android/app/src/main/java/com/example/netflix/User.java
+++ src/android/app/src/main/java/com/example/netflix/User.java
@@ -13,19 +13,11 @@ public class User {
    @NonNull
    private String id;
    private String username;
-   private ArrayList<String> movies_watched;
-   private String email;
-   private String phoneNumber;
-   private int privilegeLevel;
    private String picture;
 
-    public User(@NonNull String id, String username, String email, String phoneNumber, int privilegeLevel, String picture) {
+    public User(@NonNull String id, String username, String picture) {
        this.id = id;
        this.username = username;
-       this.email = email;
-       this.phoneNumber = phoneNumber;
-       this.movies_watched = new ArrayList<>();
-       this.privilegeLevel = privilegeLevel;
        this.picture = picture;
     }
 
@@ -33,46 +25,22 @@ public class User {
       return username;
    }
 
-   public String getEmail() {
-      return email;
-   }
-
-   public String getPhoneNumber() {
-      return phoneNumber;
-   }
-
-   public int getPrivilegeLevel() {
-      return privilegeLevel;
-   }
-
    public String getPicture() {
       return picture;
    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
 
-   public void setPassword(String password) {
-      this.password = password;
-   }
 
    @NonNull
    public String getId() {
       return id;
    }
 
-   public void setPhoneNumber(String phoneNumber) {
-      this.phoneNumber = phoneNumber;
-   }
 
    public void setPicture(String picture) {
       this.picture = picture;
    }
 
-   public void setPrivilegeLevel(int privilegeLevel) {
-      this.privilegeLevel = privilegeLevel;
-   }
 
    public void setUsername(String username) {
       this.username = username;
@@ -80,14 +48,6 @@ public class User {
 
    public void setId(@NonNull String id) {
       this.id = id;
-   }
-
-   public ArrayList<String> getMovies_watched() {
-      return movies_watched;
-   }
-
-   public void setMovies_watched(ArrayList<String> movies_watched) {
-      this.movies_watched = movies_watched;
    }
 }
 
